5991fb3c2f2636e0a3bcc6bb1e3fd25394c137f4
Dario Lopez Padial <dario.lopez@affirm.com>
2024-02-05 09:53:15 +0100

    Fix schedule jobs to use just one initialization

---
+++ src/etls/jobs.py
@@ -0,0 +1,21 @@
+import time
+
+import schedule
+
+from src.etls.boe.load import today as boe_today
+from src.etls.bopz.load import today as bopz_today
+from src.etls.bocm.load import today as bocm_today
+from src.initialize import initialize_app
+
+
+INIT_OBJECTS = initialize_app()
+
+
+schedule.every().day.at("11:00").do(boe_today, init_objects=INIT_OBJECTS)
+schedule.every().day.at("11:05").do(bopz_today, init_objects=INIT_OBJECTS)
+schedule.every().day.at("11:10").do(bocm_today, init_objects=INIT_OBJECTS)
+# TODO: monthly jobs
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
